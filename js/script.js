bf4828420e319506dfc81aef362463042bb41536
anthony_f <afanous@hotmail.com>
2023-07-01 11:49:17 -0400

    JS completed for working random image button

--- js/script.js
+++ js/script.js
@@ -3,10 +3,30 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function (){
+// fetch random image API for 100 images
+const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
-}
+    randomImage = selectRandomImage(images);
+};
 
-getImage();
+// random index generator from 0 to 99
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+
+// function to display the image
+const displayImage = function(randomImage){
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+// event listener for button to show image
+button.addEventListener("click", function(e){
+    getImage()
+})
